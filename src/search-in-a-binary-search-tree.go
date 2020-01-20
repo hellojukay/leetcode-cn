4d40dfc1a3c2b6d43a94e3eb368fec3389348fc1
licong <licong@qianxin.com>
2020-01-20 14:36:30 +0800

    search-in-a-binary-search-tree.go

---
+++ src/search-in-a-binary-search-tree.go
@@ -0,0 +1,23 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == val {
+		return root
+	}
+	if val < root.Val {
+		return searchBST(root.Left, val)
+	}
+	return searchBST(root.Right, val)
+}
