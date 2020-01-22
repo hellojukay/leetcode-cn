4a9a23e0a4df367b09958105f3f058fa86b9da54
licong <licong@qianxin.com>
2020-01-22 19:02:27 +0800

    binary-tree-inorder-traversal.go

---
+++ src/binary-tree-inorder-traversal.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
+
+/**
+ * Definition for a binary tree node.
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
+func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	var result []int
+	result = append(result, inorderTraversal(root.Left)...)
+	result = append(result, root.Val)
+	result = append(result, inorderTraversal(root.Right)...)
+	return result
+}
