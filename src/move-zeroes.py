e99c10d27f9715fa8ab189e660790fa6b8ec8111
licong <licong@qianxin.com>
2020-07-06 13:57:39 +0800

    add move-zeroes.py

---
+++ src/move-zeroes.py
@@ -0,0 +1,20 @@
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # count 0 
+        for index in range(len(nums)):
+            if nums[index] != 0 :
+                continue
+            j = index + 1
+            # 找到不为 0 的数来补充
+            while j < len(nums):
+                if nums[j] != 0:
+                    tmp = nums[j]
+                    nums[j] = 0
+                    nums[index] = tmp
+                    break
+                j = j + 1
+
