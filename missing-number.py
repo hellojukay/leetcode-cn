7be0f953d0fc98f4078892c39844c30157f54669
licong <licong@qianxin.com>
2020-07-09 16:35:05 +0800

    add missing-number.py

---
+++ missing-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = 0
+        for i in nums:
+            result = result ^ i
+        for i in range(len(nums)+1):
+            result = result ^ i
+        return result
