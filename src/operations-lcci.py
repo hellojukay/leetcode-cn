2a849bea269e7f3baaa1a118c4e097f0050385ca
hellojukay <licong@qianxin.com>
2020-07-11 18:03:05 +0800

    add operations-lcci.py

---
+++ src/operations-lcci.py
@@ -0,0 +1,19 @@
+#https://leetcode-cn.com/problems/operations-lcci/
+class Operations:
+
+    def __init__(self):
+        pass
+
+    def minus(self, a: int, b: int) -> int:
+        return a - b
+    def multiply(self, a: int, b: int) -> int:
+        return a * b
+    def divide(self, a: int, b: int) -> int:
+        return int(a / b)
+
+# Your Operations object will be instantiated and called as such:
+# obj = Operations()
+# param_1 = obj.minus(a,b)
+# param_2 = obj.multiply(a,b)
+# param_3 = obj.divide(a,b)
+
