4efb6a1710b31579f396e096f47058c39e0b5549
licong <licong@qianxin.com>
2020-07-14 11:11:53 +0800

    add partition-list.go

---
+++ src/partition-list.go
@@ -0,0 +1,39 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+//https://leetcode-cn.com/problems/partition-list/
+func partition(head *ListNode, x int) *ListNode {
+	if head == nil {
+		return nil
+	}
+	var lt []*ListNode
+	var gt []*ListNode
+	for {
+		if head == nil {
+			break
+		}
+		if head.Val < x {
+			lt = append(lt, head)
+		} else {
+			gt = append(gt, head)
+		}
+		head = head.Next
+	}
+	var list = append(lt, gt...)
+	var p *ListNode
+	head = list[0]
+	p = head
+	for i, node := range list {
+		if i == 0 {
+			continue
+		}
+		p.Next = node
+		p = p.Next
+	}
+	p.Next = nil
+	return head
+}
