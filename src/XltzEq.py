971e9e42874d99dc6abd37eee7e47535b9f291a4
hellojukay <licong@qianxin.com>
2021-12-23 17:43:47 +0800

    add https://leetcode-cn.com/problems/XltzEq/

---
+++ src/XltzEq.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        import re
+        result = ""
+        for c in s:
+            if str(c).isalnum()
+            result = result + str(c)
+        return result.lower() == result.lower()[::-1]
