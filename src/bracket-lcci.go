900250a4e995149146a178ae8edb551c6969d3b5
hellojukay <hellojukay@163.com>
2022-01-03 11:27:12 +0800

    https://leetcode-cn.com/problems/bracket-lcci/

---
+++ src/bracket-lcci.go
@@ -0,0 +1,20 @@
+func generateParenthesis(n int) []string {
+	var result []string
+	dfs("", 0, 0, n, &result)
+	return result
+}
+
+func dfs(current string, left int, right int, n int, result *[]string) {
+	if left == n && right == n {
+		*result = append(*result, current)
+		return
+	}
+	if right > left {
+		return
+	}
+	if left > n || left > n {
+		return
+	}
+	dfs(current+"(", left+1, right, n, result)
+	dfs(current+")", left, right+1, n, result)
+}
