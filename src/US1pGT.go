ff654b1e1a5bdaceeed8ba741c89806a2f565d85
hellojukay <hellojukay@163.com>
2022-01-04 09:34:20 +0800

    https://leetcode-cn.com/problems/US1pGT/

---
+++ src/US1pGT.go
@@ -0,0 +1,44 @@
+package main
+
+type MagicDictionary struct {
+	dictionary *[]string
+}
+
+/** Initialize your data structure here. */
+func Constructor() MagicDictionary {
+	return MagicDictionary{}
+}
+
+func (this *MagicDictionary) BuildDict(dictionary []string) {
+	this.dictionary = &dictionary
+}
+
+func (this *MagicDictionary) Search(searchWord string) bool {
+	for _, word := range *this.dictionary {
+		var count int = 0
+		if len(word) != len(searchWord) {
+			continue
+		}
+		var i = 0
+		for {
+			if i == len(word) {
+				break
+			}
+			if word[i] != searchWord[i] {
+				count = count + 1
+			}
+			i = i + 1
+		}
+		if count == 1 {
+			return true
+		}
+	}
+	return false
+}
+
+/**
+ * Your MagicDictionary object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.BuildDict(dictionary);
+ * param_2 := obj.Search(searchWord);
+ */
