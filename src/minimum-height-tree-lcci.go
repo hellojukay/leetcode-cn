f7de0c7fb3d5f37f783228ec006a9f2c3a937a33
licong <licong@qianxin.com>
2023-01-10 17:52:04 +0800

    add https://leetcode.cn/problems/minimum-height-tree-lcci/

---
+++ src/minimum-height-tree-lcci.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	var mid = len(nums) / 2
+	var root = &TreeNode{
+		Val: nums[mid],
+	}
+	root.Left = sortedArrayToBST(nums[:mid])
+	if mid+1 < len(nums) {
+		root.Right = sortedArrayToBST(nums[mid+1:])
+	}
+	return root
+}
