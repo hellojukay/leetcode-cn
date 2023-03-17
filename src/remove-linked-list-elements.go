3736c23ee0c13a29ae31c95ecc1e5749c0f86fc2
hellojukay <hellojukay@163.com>
2023-03-17 13:07:46 +0800

    update

--- src/remove-linked-list-elements.go
+++ src/remove-linked-list-elements.go
@@ -37,3 +37,18 @@ func removeElements(head *ListNode, val int) *ListNode {
 	}
 	return result
 }
+
+// 使用虚拟头结点
+func removeElements2(head *ListNode, val int) *ListNode {
+	var dummyHead = new(ListNode)
+	dummyHead.Next = head
+	var current = dummyHead
+	for current != nil && current.Next != nil {
+		if current.Next.Val == val {
+			current.Next = current.Next.Next
+		} else {
+			current = current.Next
+		}
+	}
+	return dummyHead.Next
+}
